32ee257042700a487be7335c3fd635e2b5926fdd
Anthony Tilelli <anthony@tilelli.me>
2020-01-15 05:01:02 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,8 +1,6 @@
-class AddCatchphraseTocharacter < ActiveRecord::Migration[5.1]
-  #to make an additive change to a schema we create
-  #a new migration, and then in the change method,
-  #we make the change.
+class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
+  #define a change method in which to do the migration
   def change
-   add_column :characters, :catchphrase, :string
+    add_column :characters, :catchphrase, :string
   end
 end
