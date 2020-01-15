c54a718fc95e09c88550c64d77211264b065ce1d
Anthony Tilelli <anthony@tilelli.me>
2020-01-15 05:01:47 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -3,8 +3,3 @@ class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
     add_column :characters, :catchphrase, :string
   end
 end
-class AddCatchphraseTocharacter < ActiveRecord::Migration[5.1]
-  def change
-   add_column :characters, :catchphrase, :string
-  end
-end
