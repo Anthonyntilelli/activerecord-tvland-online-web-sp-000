ce90937bff570601412bab114843e32aedd615cc
Anthony Tilelli <anthony@tilelli.me>
2020-01-15 05:22:35 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,10 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
+    characters.collect do |character|
+      character.name + '-' + character.show.name
+    end
+  end
   end
   
 end
