60a964c9558d19c1ad96974c0935ab31a54c9d74
Anthony Tilelli <anthony@tilelli.me>
2020-01-15 05:28:59 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,8 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
-    self.actors.map{|actor| actor.full_name}
+    self.actors.collect  do |actor| 
+      actor.full_name
+    end
   end
 end
